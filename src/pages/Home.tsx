be5530a580685001321108415a4fbd4cd3235ade
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-03-16 13:05:02 -0400

    chore: removed temporary design/framework

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Link } from 'react-router-dom';
 import Banner from '../components/banner/Banner';
+import NotesBanner from '../components/notesBanner/NotesBanner';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Banner />
-      <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-        <h1>Welcome to Your Notes App</h1>
-        <p>This is the home page. You can search and view your notes by clicking below.</p>
-        <Link
-          to="/notes"
-          style={{
-            padding: '0.5rem 1rem',
-            background: '#007acc',
-            color: '#fff',
-            textDecoration: 'none',
-            borderRadius: '4px',
-          }}
-        >
-          Go to Notes
-        </Link>
-      </div>
+      <NotesBanner />
     </div>
   );
 };
